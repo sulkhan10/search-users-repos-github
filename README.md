39a4fb188b6bd3fe98b0b083d09c41c431fdfda9
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-03-28 13:57:39 +0700

    feat : add features

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-#GitHub User Search App
+# GitHub User Search App
 This is a simple React app that allows you to search for GitHub users and display their repositories.
 
-#Installation
-Clone the repository
-Install dependencies by running npm install
-Create a .env file in the root of the project and add your GitHub Personal Access Token as REACT_APP_GITHUB_TOKEN=your_token_here
-Start the development server by running npm start
-#Usage
-Enter the GitHub username you want to search in the search bar
-Click on the Search button or press the Enter key to start the search
-The results will be displayed as a list of users, each with their repositories.
-Click on the Show Repos button to display the user's repositories
-Click on the Hide Repos button to hide the user's repositories
-Use the pagination buttons to navigate through the pages of the search results.
-#License
+# Installation
+1. Clone the repository
+2. Install dependencies by running npm install
+3. Create a .env file in the root of the project and add your GitHub Personal Access Token as REACT_APP_GITHUB_TOKEN=your_token_here
+4. Start the development server by running npm start
+# Usage
+1. Enter the GitHub username you want to search in the search bar
+2. Click on the Search button or press the Enter key to start the search
+3. The results will be displayed as a list of users, each with their repositories.
+4. Click on the Show Repos button to display the user's repositories
+5. Click on the Hide Repos button to hide the user's repositories
+6. Use the pagination buttons to navigate through the pages of the search results.
+# License
 This project is licensed under the MIT License.
---
+++ src/App.tsx
@@ -0,0 +1,144 @@
+import { useState, useEffect } from "react";
+import { Octokit } from "octokit";
+import "./App.css"
+import { AiFillStar } from 'react-icons/ai';
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_GITHUB_TOKEN || "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",
+});
+
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pages, setPages] = useState(1);
+  const [repoDropdown, setRepoDropdown] = useState<{ [key: string]: boolean }>(
+    {}
+  );
+
+  const searchUsers = async () => {
+    const { data } = await octokit.rest.search.users({
+      q: searchQuery,
+      per_page: 5,
+      page: currentPage,
+    });
+    if (data.total_count<=30) {
+      setPages(Math.ceil(data.total_count / 5));
+    }else{setPages(6)}
+    const users: any = await Promise.all(
+      data.items.map(async (item: any) => {
+        const { data } = await octokit.rest.repos.listForUser({
+          username: item.login,
+        });
+        return { name: item.login, repos: data };
+      })
+    );
+    setUsers(users);
+    setRepoDropdown({});
+  };
+
+  const handleSearchQueryChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const toggleRepoDropdown = (name: string) => {
+    setRepoDropdown((prev) => ({ ...prev, [name]: !prev[name] }));
+  };
+
+  useEffect(() => {
+    searchUsers();
+  }, [currentPage]);
+
+  return (
+    <div className="container mx-5 my-5">
+      <div className="mb-3">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchQueryChange}
+          className="px-4 py-2 border border-gray-400 rounded-md w-full"
+          placeholder="Search username"
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full my-4"
+          onClick={searchUsers}
+        >
+          Search
+        </button>
+      </div>
+      {users.map((user: any) => (
+        <div key={user.name} className="bg-white rounded-lg shadow-md my-3">
+          <div className="p-4 flex justify-between items-center">
+            <h2 className="text-lg font-medium">{user.name}</h2>
+            <button
+              className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-lg shadow-md"
+              onClick={() => toggleRepoDropdown(user.name)}
+            >
+              {repoDropdown[user.name] ? "Hide Repos" : "Show Repos"}
+            </button>
+          </div>
+          {repoDropdown[user.name] && (
+            <div className="px-4 py-2">
+              {user.repos.map((repo: any) => (
+                <div key={repo.name} className="py-2">
+                  <div className="flex justify-between w-full">
+                    <div>
+                      <a href={repo.html_url} className="text-blue-500 ">
+                        {repo.name}
+                      </a>
+                    </div>
+                    <div className="flex items-center">
+                    <AiFillStar/>
+                      <p>{` `} {repo.stargazers_count}</p>
+                      </div>
+                  </div>
+                  <p>{` `}{repo.description}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+      {users.length !== 0 && (<div className="flex justify-center w-full">
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg shadow-md mr-2 ${
+            currentPage === 1 && "opacity-50 cursor-not-allowed"
+          }`}
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        {[...Array(pages)].map((_, index) => (
+          <button
+            key={index}
+            className={`bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 rounded-lg shadow-md mx-1 ${
+              index + 1 == currentPage && "bg-sky-300 text-gray-500"
+            }`}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg shadow-md ml-2 ${
+            users.length < 5 && "opacity-50 cursor-not-allowed"
+          }`}
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={users.length < 5}
+        >
+          Next
+        </button>
+      </div>)}
+      
+    </div>
+  );
+}
+
+export default App;
